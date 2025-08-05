36a8922324f6a207b7007549aa05e5b80414d09d
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2025-08-05 13:09:10 +0530

    updated project url

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,18 +9,18 @@ const Projects = () => {
 
   return (
     <div className='w-[95%] mx-auto p-4   min-h-screen'>
-           <div className='flex items-center justify-center gap-[30px] font-bold rounded-full bg-slate-700 w-[90%] mx-auto my-[50px]'>
-                <h1 className='text-2xl   '>My Projects </h1>
-            </div>
-    <div className='flex flex-wrap  gap-[25px]  items-center w-full mx-auto justify-center'>
-           
-    <Project image={blogifyImg}  title={"Blogify"} key={"Blogify"} description={"A Full stack Blog Website built using React,Node,Express and MongoDb"} link={"https://my-blogify-app.netlify.app"}/>
+      <div className='flex items-center justify-center gap-[30px] font-bold rounded-full bg-slate-700 w-[90%] mx-auto my-[50px]'>
+        <h1 className='text-2xl   '>My Projects </h1>
+      </div>
+      <div className='flex flex-wrap  gap-[25px]  items-center w-full mx-auto justify-center'>
 
-    <Project image={taskImg}  title={"Task Management App"} key={"Task "} description={"A Full stack Task Management Website built using Next.js,Node,Express and MongoDb "} link={"https://taskmanagement-webapp.vercel.app"}/>
+        <Project image={blogifyImg} title={"Blogify"} key={"Blogify"} description={"A Full stack Blog Website built using React,Node,Express and MongoDb"} link={"https://my-blogify-app.netlify.app"} />
 
-    <Project image={heritageImg}  title={"Heritage Website"} key={"Heritage"} description={"A Website built using React and Tailwind that gives the information about the 'City of Joy' Kolkata "} link={"https://heritageofkolkata.vercel.app"}/>
+        <Project image={taskImg} title={"Task Management App"} key={"Task "} description={"A Full stack Task Management Website built using Next.js,Node,Express and MongoDb "} link={"https://taskify-web-app-five.vercel.app/"} />
 
-           </div>
+        <Project image={heritageImg} title={"Heritage Website"} key={"Heritage"} description={"A Website built using React and Tailwind that gives the information about the 'City of Joy' Kolkata "} link={"https://heritageofkolkata.vercel.app"} />
+
+      </div>
     </div>
   )
 }
